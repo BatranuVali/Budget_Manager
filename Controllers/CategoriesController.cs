7e8d59436680c9c5a8bc8a7d8a9f475df22c0a38
BatranuVali <119863256+BatranuVali@users.noreply.github.com>
2024-04-18 13:35:26 +0300

    Combine Edit and Create into AddOrEdit

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -44,18 +44,23 @@ namespace Budget_Manager.Controllers
             return View(category);
         }
 
-        // GET: Categories/Create
-        public IActionResult Create()
+        // GET: Categories/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
         {
-            return View();
+            if(id==0)
+            {
+                return View(new Category());
+            }
+            else return View(_context.Categories.Find(id));
         }
 
-        // POST: Categories/Create
+
+        // POST: Categories/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
@@ -66,21 +71,6 @@ namespace Budget_Manager.Controllers
             return View(category);
         }
 
-        // GET: Categories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
 
         // POST: Categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8 / V1NBaF5cXmZCekx0QHxbf1x0ZFRMY1RbQXRPMyBoS35RckVnWHdecXddQ2JaU01 +\r\n");
+
 //DI
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
